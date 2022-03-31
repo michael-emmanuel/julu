adf060d4a9eb0a5b672826a1ca75241f2028fa0e
Michael Emmanuel <michael.emmanuel732@gmail.com>
2022-03-30 22:42:10 -0400

    set up nextjs, create Header Component

---
+++ julu/pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Header from '../components/Header';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Julu</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* Header */}
+      <Header />
+
+      {/* Nav */}
+
+      {/* Results */}
+    </div>
+  )
+}
