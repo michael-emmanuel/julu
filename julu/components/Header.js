2abd336c39691513165357f1d68d10799a4c6965
Michael Emmanuel <michael.emmanuel732@gmail.com>
2022-04-02 19:32:47 -0400

    add nav bar, add title, add gradient

--- julu/components/Header.js
+++ julu/components/Header.js
@@ -1,7 +1,32 @@
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon
+} from "@heroicons/react/outline";
+import Image from "next/image";
+import HeaderItem from "./HeaderItem";
+
 function Header() {
   return (
-    <header className="">
-      <h1>This is the header</h1>
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title='HOME' Icon={HomeIcon} />
+        <HeaderItem title='TRENDING' Icon={LightningBoltIcon} />
+        <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} />
+        <HeaderItem title='COLLECTIONS' Icon={CollectionIcon} />
+        <HeaderItem title='SEARCH' Icon={SearchIcon} />
+        <HeaderItem title='ACCOUNT' Icon={UserIcon} />
+      </div>
+      <Image 
+        src='https://links.papareact.com/ua6' 
+        className="object-contain"
+        width={200} 
+        height={100}
+        alt={"foo"}
+        />
     </header>
   )
 }
--- julu/tailwind.config.js
+++ julu/tailwind.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   mode: 'jit',
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false,
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require('tailwind-scrollbar-hide')],
+};
